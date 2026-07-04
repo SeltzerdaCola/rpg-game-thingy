f56f5169cd906d8c864867b43471fa8787f08d37
Seltzer <renchjosefT@gmail.com>
2026-07-04 17:36:30 +0800

    Tweaks

--- scripts/loaders/tilesprites.py
+++ scripts/loaders/tilesprites.py
@@ -98,7 +98,7 @@ class TileGroup(p.sprite.Group):
 		if new_layer:
 			self.sorted_sprites = {k: v for k, v in sorted(self.sorted_sprites.items())}
 	def draw_all(self, game):
-		
+		blit_counter = 0
 		
 		for l in self.sorted_sprites:
 			for s in self.sorted_sprites[l]:
@@ -107,6 +107,8 @@ class TileGroup(p.sprite.Group):
 					self.sorted_sprites[l].remove(s)
 				if bool(s.visible):
 					game.camera.tile_blit(s)
+					blit_counter += 1
+		print(f"Tiles blitted: {blit_counter}")
 		
 class UiGroup(p.sprite.Group):
 	def __init__(self):
@@ -116,4 +118,6 @@ class UiGroup(p.sprite.Group):
 		
 	def draw_all(self, game):
 		
-		[game.camera.ui_blit(s) for s in self.sprites() if bool(s.visible)]
+		for s in self.sprites():
+			if bool(s.visible):
+				game.camera.ui_blit(s)
